946ff5fe33c1db4145e32fc3a43d28dab5cad92d
Nik <nik.kholin@gmail.com>
2016-10-13 16:13:35 +0300

    added message destruction

--- app.rb
+++ app.rb
@@ -4,16 +4,18 @@ require_relative 'models/message'
 
 get '/' do
   @messages = Message.all
-  # @message = Message.create(text: 'params[text]', urlsafe: SecureRandom.urlsafe_base64)
-  # @message = 2
   erb :message_new
 end
 
 post '/message' do
   message = Message.new(text: params['text'], urlsafe: SecureRandom.urlsafe_base64)
-  # message = Message.new(text: 'params[text]', urlsafe: SecureRandom.urlsafe_base64)
   if params['destruction_option'] == 'one_link_visit'
-    message.visits_remaining = 1
+    message.visits_remaining = 1 + 1 # +1 because it shows message right after creation
+  else
+    Thread.new do
+      sleep 1.hour
+      message.delete
+    end
   end
   message.save
   redirect to("/message/#{message.urlsafe}")
@@ -21,5 +23,16 @@ end
 
 get '/message/:urlsafe' do
   @message = Message.where(urlsafe: params['urlsafe']).last
+  if @message.nil?
+    redirect to('/')
+  else
+    if @message.destroyed_via_link_visits?
+      @message.visits_remaining -= 1
+      @message.save
+      if @message.visits_remaining == 0
+        @message.delete
+      end
+    end
+  end
   erb :message_show
 end
--- models/message.rb
+++ models/message.rb
@@ -1,5 +1,18 @@
-require 'sinatra/activerecord'
-
 class Message < ActiveRecord::Base
+
+  def destroyed_after_time?
+    visits_remaining.nil?
+  end
+
+  def destroyed_via_link_visits?
+    !destroyed_after_time?
+  end
+
+  def time_remaining
+    if destroyed_after_time?
+      seconds = 1.hour - (Time.now - created_at)
+      Time.at(seconds).strftime('%H:%M:%S')
+    end
+  end
 
 end
